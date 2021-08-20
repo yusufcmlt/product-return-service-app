b4e5a0045467d9b97e5cb503dd9ee5b5a6988f8d
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-20 17:56:06 +0300

    feat: add language context

---
+++ src/utils/languages.js
@@ -0,0 +1,12 @@
+const languages = {
+  tr: {
+    newReturn: 'Yeni İade',
+    checkReturnStatus: 'İade Durum Sorgula',
+  },
+  en: {
+    newReturn: 'New Return',
+    checkReturnStatus: 'Status of Your Return',
+  },
+};
+
+export default languages;
