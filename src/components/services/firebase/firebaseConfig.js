6fd1d31f12752779f1f3a87d535cf17aba960a79
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-26 00:23:32 +0300

    feat: Add firebase and utils

---
+++ src/components/services/firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDVOWbLVsUqdhHD0L8EX8V7tPssz0nqX4U',
+  authDomain: 'innovance-case-yusuf.firebaseapp.com',
+  projectId: 'innovance-case-yusuf',
+  storageBucket: 'innovance-case-yusuf.appspot.com',
+  messagingSenderId: '66141914309',
+  appId: '1:66141914309:web:5b59fab1a68a2d93e8dcac',
+  measurementId: 'G-7XCQPSEZN3',
+};
+
+export default firebaseConfig;
