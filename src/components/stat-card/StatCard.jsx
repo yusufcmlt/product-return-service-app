afbf76c7438222b78257438068efac4978607f7c
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-27 19:14:03 +0300

    fix: Change statcard role

--- src/components/stat-card/StatCard.jsx
+++ src/components/stat-card/StatCard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './StatCard.style.scss';
 
-export default function StatCard({ title, detail, color = 'gray' }) {
+export default function StatCard({ title, detail, color = 'gray', onClick = () => {} }) {
   return (
-    <div className={`stat-card stat-card--${color}`}>
+    <button type="button" className={`stat-card stat-card--${color}`} onClick={onClick}>
       <span className="stat-card__title">{title}</span>
       <span className="stat-card__detail">{detail}</span>
-    </div>
+    </button>
   );
 }
