bd23da2ddc93ddc3fd22811f20fadd1fea892138
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-28 00:26:50 +0300

    fix: Fix option values shown in information

--- src/pages/ticket-success/components/ProcessDetail.jsx
+++ src/pages/ticket-success/components/ProcessDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import InfoText from 'components/info-text/InfoText';
+import { ticketFormReasonMessages } from 'utils/form-utils/formConstants';
 
 export default function ProcessDetail({ data, itemLink }) {
   return (
@@ -9,7 +10,7 @@ export default function ProcessDetail({ data, itemLink }) {
       <InfoText title="Ad" detail={data.ticketFirstName} />
       <InfoText title="Soyad" detail={data.ticketLastName} />
       <InfoText title="Ürün Kodu" detail={data.ticketItemCode} />
-      <InfoText title="İade Nedeni" detail={data.ticketReason} />
+      <InfoText title="İade Nedeni" detail={ticketFormReasonMessages[data.ticketReason]} />
       <InfoText title="İade Detayı" detail={data.ticketReasonDetail} />
       <InfoText title="İşlem Tarihi" detail={data.ticketCreatedAt.toLocaleDateString()} />
     </div>
