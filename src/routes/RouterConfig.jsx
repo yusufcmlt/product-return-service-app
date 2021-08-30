af2586c875d89987349984fd51ba6bd610ab435d
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-30 13:58:53 +0300

    refactor: Use params conditionally

--- src/routes/RouterConfig.jsx
+++ src/routes/RouterConfig.jsx
@@ -1,18 +1,16 @@
-/* eslint-disable */
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
-import { AdminTicketProvider } from 'contexts/AdminTicketsContext';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from 'pages/home/Home';
-import NewReturn from 'pages/new-return/NewReturn';
+import NewTicket from 'pages/new-ticket/NewTicket';
 import TicketSuccess from 'pages/ticket-success/TicketSuccess';
 import CheckTicket from 'pages/check-ticket/CheckTicket';
 import TicketResponse from 'pages/ticket-response/TicketResponse';
 import Admin from 'pages/admin/Admin';
 import AdminDashBoard from 'pages/admin-dashboard/AdminDashBoard';
 
-// import NotFoundRoute from './NotFoundRoute';
 import PrivateRoute from './PrivateRoute';
+import NotFoundRoute from './NotFoundRoute';
 
 import {
   ADMIN,
@@ -23,24 +21,21 @@ import {
   PUBLIC_TICKET_ITEM,
   TICKET_SUCCESS,
 } from './RouteContstants';
-import NotFoundRoute from './NotFoundRoute';
 
 export default function RouterConfig() {
   return (
     <Switch>
       <Route exact path={HOMEPAGE} component={Home} />
-      <Route exact path={NEW_TICKET} component={NewReturn} />
+      <Route exact path={NEW_TICKET} component={NewTicket} />
       <Route exact path={TICKET_SUCCESS} component={TicketSuccess} />
       <Route exact path={CHECK_TICKET} component={CheckTicket} />
-      <Route exact path={PUBLIC_TICKET_ITEM} component={TicketResponse} />
-      <Route exact path={`${PUBLIC_TICKET_ITEM}/:id`} component={TicketResponse} />
+      <Route exact path={`${PUBLIC_TICKET_ITEM}/:id?`} component={TicketResponse} />
       <Route exact path={ADMIN} component={Admin} />
-      <PrivateRoute exact path={ADMIN_TICKET_LIST}>
+      {/* Private route with optional id parameter */}
+      <PrivateRoute exact path={`${ADMIN_TICKET_LIST}/:id?`}>
         <AdminDashBoard />
       </PrivateRoute>
-      <PrivateRoute exact path={`${ADMIN_TICKET_LIST}/:id`}>
-        <AdminDashBoard />
-      </PrivateRoute>
+      {/* Private route with optional id parameter */}
       <Route path="*">
         <NotFoundRoute />
       </Route>
