f542df442e56911297f14dde0a00e9795086058c
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-30 13:53:15 +0300

    refactor: Changes on statcard props,test

--- src/components/stat-card/StatCard.jsx
+++ src/components/stat-card/StatCard.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './StatCard.style.scss';
 
-export default function StatCard({ title, detail, color = 'gray', onClick = () => {} }) {
+export default function StatCard({ title, detail, color = 'gray', ...restProps }) {
   return (
-    <button type="button" className={`stat-card stat-card--${color}`} onClick={onClick}>
+    <button type="button" className={`stat-card stat-card--${color}`} {...restProps}>
       <span className="stat-card__title">{title}</span>
       <span className="stat-card__detail">{detail}</span>
     </button>
