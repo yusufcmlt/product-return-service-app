744d2220d342d5398fe9ad64a921be6f53bdac9d
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-30 13:52:32 +0300

    refactor: Changes on info text component  props, test

--- src/components/info-text/InfoText.jsx
+++ src/components/info-text/InfoText.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 import './InfoText.style.scss';
 
-const InfoText = ({ title = '', detail = '' }) => (
-  <span className="info">
+const InfoText = ({ title = '', detail = '', ...restProps }) => (
+  <span className="info" {...restProps}>
     <h6 className="info__title">{title}</h6>
-
     <p className="info__detail">{detail}</p>
   </span>
 );
