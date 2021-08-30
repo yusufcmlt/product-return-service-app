41644cf4fefcb714af13a7e5ace7c8ce7e17674f
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-30 13:55:59 +0300

    refactor: Change ticket block component to use ticket data without modifications

--- src/components/ticket-block/TicketBlock.jsx
+++ src/components/ticket-block/TicketBlock.jsx
@@ -1,20 +1,21 @@
 /* eslint-disable */
 import React from 'react';
-import { ticketFormReasonMessages, ticketStatusMessage } from 'utils/form-utils/formConstants';
+import { ticketStatusMessage } from 'utils/form-utils/formConstants';
+import { getTicketStatusColor } from 'utils/ticket-data-utils/ticketDataUtils';
 
 import './TicketBlock.style.scss';
 
 export default function TicketBlock({ ticketData, ...props }) {
   const { id, ticketFirstName, ticketLastName, ticketReason, ticketStatus } = ticketData;
 
+  const statusColor = getTicketStatusColor(ticketStatus);
+
   return (
     <div role="button" className="ticket-block" {...props}>
       <span className="ticket-block__col">{id}</span>
       <span className="ticket-block__col">{`${ticketFirstName} ${ticketLastName}`}</span>
-      <span className="ticket-block__col">{ticketFormReasonMessages[ticketReason]}</span>
-      <span className={`ticket-block__col ticket-block__col--${ticketStatus}`}>
-        {ticketStatusMessage[ticketStatus]}
-      </span>
+      <span className="ticket-block__col">{ticketReason}</span>
+      <span className={`ticket-block__col ticket-block__col--${statusColor}`}>{ticketStatus}</span>
     </div>
   );
 }
